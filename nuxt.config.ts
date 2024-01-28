ea018484f13cc2c9dc58785868c2081b5c9d2340
Luiz Barreto <llbarre@hotmail.com>
2024-01-28 09:50:27 -0600

    Add @vueuse/nuxt module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: [
+    "@nuxt/ui", // https://ui.nuxt.com/
+    "@vueuse/nuxt", // https://nuxt.com/modules/vueuse
+  ],
 })
