d37d811673bc11c1e6c3c01bb64dd582d2c9385f
Luiz Barreto <llbarre@hotmail.com>
2024-01-28 10:25:40 -0600

    Remove unused script and style tags

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,5 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
   <UContainer class="py-8">
     About page
   </UContainer>
 </template>
-
-<style>
-
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,5 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
   <UContainer class="py-8 rounded-lg border border-dotted ">
     Landing page
   </UContainer>
 </template>
-
-<style>
-
-</style>
