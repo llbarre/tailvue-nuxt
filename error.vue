981ee4be4aae19c5f7e525d45d52759972ff3b4a
Luiz Barreto <llbarre@hotmail.com>
2024-01-28 14:30:11 -0600

    Add error.vue component

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup>
+const error = useError()
+function handleError() {
+  clearError({
+    redirect:
+        "/",
+  })
+}
+</script>
+
+<template>
+  <NuxtLayout>
+    <UContainer>
+      <div class="flex flex-col min-h-[40vh] h-full items-center justify-center space-y-4">
+        <h1 class="text-3xl font-bold">
+          {{ error.statusCode }}
+        </h1>
+        <p>{{ error.statusMessage }}.</p>
+      </div>
+    </UContainer>
+  </NuxtLayout>
+</template>
